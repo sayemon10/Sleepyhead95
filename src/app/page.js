825248a7fb595d602bf91174bf1ff7946a614aef
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2023-09-09 11:22:25 +0200

    added main content, fixed layout issues

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "../components/header/Header";
 import Footer from "@/components/footer/Footer";
+import Main from "@/components/main/Main";
 
 export default function Home() {
   return (
     <body>
       <Header />
-
+      <Main />
       <Footer />
     </body>
   );
---
+++ src/components/main/Main.js
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import styles from "./main.module.css";
+import Link from "next/link";
+
+export default function Main() {
+  return (
+    <main id="redirect" className={styles.main}>
+      <div className={styles.me}>
+        <Image
+          src="/portfolio picture.png"
+          alt="picture of Janka Antalová"
+          width={500}
+          height={550}
+        />
+      </div>
+      <div className={styles.aboutWrapper}>
+        <div id="about-me" className={styles.aboutMe}>
+          <h2>Hello, I&apos;m Janka!</h2>
+          <p>
+            Welcome to my portfolio. I am a Front-End developer, Graphic
+            Designer, and Illustrator from Slovakia, currently based in Italy.
+            <br />
+            <br />
+            I&apos;ve always enjoyed a good challenge, and moving from art to
+            programming has not only been huge, but also hugely rewarding.
+            Seeing my designs come to life in the form of fully-functioning
+            websites and web apps that others can use has given me an unknown
+            sense of satisfaction.
+            <br />
+            <br />I would like to keep doing that.
+          </p>
+        </div>
+        <div className={styles.workContact}>
+          <h3>Open for work</h3>
+          <p>
+            I am currently looking for job opportunities in the Front-End
+            Development and/or Graphic Design fields.&nbsp;
+            <span id="connect" className={styles.connect}>
+              Let&apos;s connect!
+            </span>
+          </p>
+          <div className={styles.contacts}>
+            <Link href="https://twitter.com/antalova_janka" target="_blank">
+              <Image
+                src="/Icons/b&w/211919_social_twitter_icon.png"
+                alt="link to Twitter profile"
+                width={20}
+                height={20}
+              />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/jana-antalov%C3%A1-6340a1173/"
+              target="_blank"
+            >
+              <Image
+                src="/Icons/b&w/1243560_linkedin_outline_social_icon.png"
+                alt="link to LinkedIn profile"
+                width={20}
+                height={20}
+                className={styles.linkedin}
+              />
+            </Link>
+            <Link href="https://github.com/Sleepyhead95" target="_blank">
+              <Image
+                src="/Icons/b&w/317712_code repository_github_repository_resource_icon.png"
+                alt="Link to GitHub profile"
+                width={20}
+                height={20}
+              />
+            </Link>
+            <Link href="mailto: januska.antalova@gmail.com">
+              <Image
+                src="/Icons/b&w/3586360_email_envelope_mail_send_icon.png"
+                alt="Link to email address"
+                width={20}
+                height={20}
+                className={styles.email}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
