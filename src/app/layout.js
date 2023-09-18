10932bf2886a31ab4f0cc17452843948ea65a992
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2023-09-18 15:42:36 +0200

    font stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,13 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <Link rel="preconnect" href="https://fonts.googleapis.com" />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <Link rel="stylesheet" href="https://use.typekit.net/nff3epz.css" />
       </Head>
       <body>{children}</body>
     </html>
