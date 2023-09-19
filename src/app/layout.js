30bdec39f297e87c74f650f00d80e799cbf2d462
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2023-09-19 11:11:19 +0200

    fixed the fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import styles from "./globals.css";
+// import { Josefin_Sans } from "next/font/google";
 import Head from "next/head";
-import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Josefin_Sans({
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Janka's Portfolio",
@@ -19,15 +20,8 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <Link rel="preconnect" href="https://fonts.googleapis.com" />
-        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <Link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <Link rel="stylesheet" href="https://use.typekit.net/nff3epz.css" />
       </Head>
-      <body>{children}</body>
+      <body className={styles.body}>{children}</body>
     </html>
   );
 }
